93a6966adfc14a0c2651718659ae05b15959900b
Krishna Suryavanshi <krishna.suryavanshiac@gmail.com>
2024-10-07 22:41:04 +0530

    Revert "initial commit" Yes This reverts commit 6e7caf193d94117b265919f11bb8ee5c63db7222.

--- PicaPool2/picapool/lib/models/button_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ButtonModel {
-  final String text;
-  final VoidCallback onTap;
-  final String? imagePath;
-
-  ButtonModel({required this.text, required this.onTap, this.imagePath});
-}
