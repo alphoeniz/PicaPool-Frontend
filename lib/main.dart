0d10ab1877d517fbee4d745078044883108781c0
Bhaskar <bhaskarmandal369@gmail.com>
2024-10-20 16:54:25 +0530

    feat: message model and socket service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:picapool/firebase_options.dart';
 import 'package:picapool/screen_router.dart';
+import 'package:picapool/screens/Public%20Chat/publicChatScreen.dart';
+import 'package:picapool/screens/chats/chat_homeScreen.dart';
+import 'package:picapool/screens/home_screen.dart';
 import 'package:picapool/screens/login_screen.dart';
 
 void main() async{
     WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env");
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
     runApp(const MyApp());
@@ -29,7 +34,7 @@ class MyApp extends StatelessWidget {
               const TextSelectionThemeData(cursorColor: Color(0xffffffff))),
       // home: const LoginScreen(),
       // home: const HomeScreen(),
-      home: const LoginScreen(),
+      home: PublicChatPage(),
       // home: const CreateCabShareScreen(),
       // home: const CreatePoolScreen(),
     );
