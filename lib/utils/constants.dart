792f11cc7199ffb88524dd976b38d3ba0e934a7b
Moin97 <moinuddin9777@gmail.com>
2024-11-18 03:23:03 +0530

    added network and api utils

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const apiUrl = "https://api.picapool.com/v2";
+  static const localApiUrl = "http://localhost:5197/v2";
+  static const createProductEndpoint = "/product";
+  static const getAllProductsEndpoint = "/product";
+  static const getAllCategoriesEndpoint = "/product-category";
+
+  // endpoints that need an ID
+  static const getProductEndoint = "product/";
+  
+}
---
+++ lib/utils/logger_helper.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:logger/logger.dart';
+
+Logger logger = Logger(printer: PrettyPrinter(), level: Logger.level);
